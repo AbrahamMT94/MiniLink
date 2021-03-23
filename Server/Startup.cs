c672994fdf66f0a1034c9ec7804e3dbaae8f8abd
AB MT <abrahammarcos@hotmail.com>
2021-03-23 15:45:07 -0500

    removed unnecesary components

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -44,15 +44,17 @@ namespace MiniLink.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MiniLinkContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+              
             }
             else
             {
+                context.Database.Migrate();
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
